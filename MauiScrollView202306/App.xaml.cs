f34e93479f9fa8585d0f17b9ef0beda7538202ec
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-06-30 22:38:03 +0200

    Clean project

---
+++ MauiScrollView202306/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiScrollView202306;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiScrollView202306/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiScrollView202306;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
